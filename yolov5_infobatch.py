42766d674f7199b9cf32770f9da2489f9a9faa57
秦紫恒 <ziheng.qin@bytedance.com>
2024-09-23 11:53:09 +0800

    yolov5_training files

---
+++ yolov5_infobatch.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov5.yaml")  # build a new model from scratch
+
+# Use the model
+model.train(data="coco.yaml", epochs=100, imgsz=640, hyp='yolov5_training.yaml')  # train the model
+metrics = model.val()
